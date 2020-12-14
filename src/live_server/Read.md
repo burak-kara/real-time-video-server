23f4ad4a224d7fef0823afbd2b6e02b5ddc52ce2
burakkkara <burak.kara@ozu.edu.tr>
2020-12-14 16:19:24 +0300

    done

---
+++ src/live_server/Read.md
@@ -0,0 +1,3 @@
+- first run the shell script named capture.sh under the video_source
+- then, run the shell script named stream.sh under the live
+- finally, run the live_server.py
--- src/live_server/live/stream.sh
+++ src/live_server/live/stream.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-VIDSOURCE="udp://192.168.1.38:5000"
-VIDEO_OPTS="-s 640x360 -c:v libx264 -b:v 512k -g 25 -sc_threshold 0 -max_muxing_queue_size 1024"
-OUTPUT_HLS="-hls_time 2 -hls_list_size 100"
-ffmpeg -i "$VIDSOURCE" -y $VIDEO_OPTS $OUTPUT_HLS stream.m3u8
+ffmpeg -i udp://192.168.1.38:5000 -y -s 640x360 -c:v libx264 -b:v 512k -g 25 -sc_threshold 0 -max_muxing_queue_size 1024 -hls_time 2 -hls_list_size 100 stream.m3u8
